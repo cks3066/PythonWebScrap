f957b9103cffaf6bcb6f4eaf20afb0406fa73e6c
Seungchan On <62434898+cks3066@users.noreply.github.com>
2022-01-18 22:29:50 +0000

    feat: extracting loaction

--- indeed.py
+++ indeed.py
@@ -10,6 +10,19 @@ def convert_string_with_comma_to_number(some_string):
     string_replaced = some_string.replace(',','')
   return int(string_replaced)
 
+def extract_job(html):
+    title = html.find("span", title=True).string
+    company = html.find("span", {"class": "companyName"})
+    if company is not None:
+      company_anchor = company.find("a")
+      if company.find("a") is not None:
+        company = company_anchor.string
+      else:
+        company = company.string
+    location = html.find("div", {"class", "companyLocation"}).string
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, 'link': f"https://kr.indeed.com/%EC%B1%84%EC%9A%A9%EB%B3%B4%EA%B8%B0?jk={job_id}&from=serp&vjs=3"}
+
 def extract_indeed_pages():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
@@ -27,16 +40,12 @@ def extract_indeed_pages():
 
 def extract_indeed_jobs(last_page):
   jobs = []
-  #for page in range(last_page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, "html.parser")
-  results = soup.find_all("td", {"class": "resultContent"})
-  for result in results:
-    title = result.find("span", title=True).string
-    company = result.find("span", {"class": "companyName"})
-    company_anchor = company.find("a")
-    if company.find("a") is not None:
-      company = company_anchor.string
-    else:
-      company = company.string
+  for page in range(last_page):
+    print(f"Scrapping page {page}")
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("a", {"class": "result"})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
   return jobs
