c9fe6d904077c2c59cdd5d42852d6e31ff5a8d9f
Seungchan On <62434898+cks3066@users.noreply.github.com>
2022-01-19 12:27:31 +0000

    fix: fix print in main.py

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ last_indeed_pages = extract_indeed_pages()
 
 indeed_jobs = extract_indeed_jobs(last_indeed_pages)
 
-for indeed_job in indeed_jobs:
-  if(indeed_job['company'] is None):
-    print(indeed_job)
+print(indeed_jobs)
